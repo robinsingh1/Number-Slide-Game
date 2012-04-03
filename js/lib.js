20984ef08e8ea08984706ab40cad9da0ee0adbce
Robin Singh <robin.singh.1991@gmail.com>
2012-04-03 15:50:52 -0400

    stuff

--- js/lib.js
+++ js/lib.js
@@ -43,12 +43,51 @@ function new_game() {
 			$('#'+parent_id_2).css({'height':'100', 'width':'110'});
 		}
 	});
+	Board.set_draggable(empty.position);
 }
 
 function alert_position(){
-	Board.set_draggable(empty.position);
+//	Board.set_draggable(empty.position);
 	//is_touching is an array of all touching divs
 	//iterate through access div within div -> set draggable
+	
+	$('#box_9').droppable({
+		over: function(event, ui) {
+			var id = $('.ui-draggable-dragging').attr("id");
+			var empty_pos = empty.position;
+			empty_pos = empty_pos.toString();
+			parent_id = $('#'+id).parent().attr('id');
+			parent_id_2 = $('#box_9').parent().attr('id');
+			console.log(parent_id);
+
+			//Offset From Top Left Corner of browser window 
+			var pos = $('#box_9').offset();
+			var pos_dragging = $('#'+id).offset();
+			console.log(pos.left);
+
+			var left = pos.left;
+			var top = pos.top;
+			$('#'+id).css({'position': 'absolute', 'top':pos_dragging.top-10, 'left':pos_dragging.left-10});
+			$('#'+id).animate({
+				top: top-10,
+				left: left-10
+			}, {
+				complete: function(){
+					var temp_html = $('#'+parent_id_2).html();
+					var temp_html2 = $('#'+parent_id).html();
+					$('#'+parent_id).html(temp_html);
+					$('#'+parent_id_2).html(temp_html2);
+					
+					$('#'+parent_id).css({'height':'100', 'width':'0'});
+					$('#'+parent_id_2).css({'height':'110', 'width':'0'});
+				}
+			});
+			$('#'+parent_id).css({'height':'110', 'width':'110'});
+			$('#'+parent_id_2).css({'height':'100', 'width':'110'});
+		}
+	});
+
+
 }
 
 window.setInterval(function () {
